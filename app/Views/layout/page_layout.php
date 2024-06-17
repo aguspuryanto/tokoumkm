6b59f32dbdbf150123980d18f144228939ea563d
Agus Puryanto <aguspuryanto@gmail.com>
2024-06-17 16:43:14 +0700

    update

--- app/Views/layout/page_layout.php
+++ app/Views/layout/page_layout.php
@@ -10,6 +10,10 @@
     <meta name="author" content="">
 
     <title>SB Admin 2 - Dashboard</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url('plugins/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
