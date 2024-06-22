85281ab35386f1f5311ac75278ca13caf39651b3
Agus Puryanto <aguspuryanto@gmail.com>
2024-06-22 19:14:11 +0700

    update

---
+++ app/Views/_partials/nav_header.php
@@ -0,0 +1,16 @@
+
+<div class="container">
+        <nav class="navbar navbar-dark bg-light justify-content-between">
+            <a class="navbar-brand" href="<?= base_url() ?>">
+                <img src="<?= (base_url('uploads/' . $page_logo)) ?? base_url('uploads/oq15SNxkJQscqbVsgZat.jpg') ?>" width="30" height="30" class="d-inline-block align-top" alt="">
+            </a>
+            <form class="form-inline">
+                <div class="input-group">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-success my-0 my-sm-0" type="submit">Search</button>
+                    </div>
+                </div>
+            </form>
+        </nav>
+    </div>
--- app/Views/main_page.php
+++ app/Views/main_page.php
@@ -10,21 +10,7 @@
     <link href="<?= base_url('css/style.css') ?>" rel="stylesheet">
 </head>
 <body>
-    <div class="container">
-        <nav class="navbar navbar-dark bg-light justify-content-between">
-            <a class="navbar-brand" href="#">
-                <img src="<?= (base_url('uploads/' . $page_logo)) ?? base_url('uploads/oq15SNxkJQscqbVsgZat.jpg') ?>" width="30" height="30" class="d-inline-block align-top" alt="">
-            </a>
-            <form class="form-inline">
-                <div class="input-group">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <div class="input-group-append">
-                        <button class="btn btn-outline-success my-0 my-sm-0" type="submit">Search</button>
-                    </div>
-                </div>
-            </form>
-        </nav>
-    </div>    
+    <?php include '_partials/nav_header.php' ?>
     
     <?php include '_partials/nav_bottom.php' ?>
 
